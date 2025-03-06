8c3becda52194a7a041dabc4363a7f175f485181
Owner <owner@owners-mbp.home>
2025-03-05 22:07:57 -0500

    chore: update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
+  output: 'export',
   images: {
     unoptimized: true,
   },
-  // 根据环境设置不同的配置
-  ...(process.env.NODE_ENV === 'production' 
-    ? {
-        basePath: '/Web',
-        assetPrefix: '/Web',
-      }
-    : {})
+  basePath: '/Web',
+  assetPrefix: '/Web/',
 };
 
 export default nextConfig; 
