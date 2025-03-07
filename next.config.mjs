c0f9fe7f72916ac2a708d34969d06f884fcb92ae
Owner <owner@owners-mbp.home>
2025-03-06 22:25:36 -0500

    fix: update next.js config for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/Web' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Web/' : '',
 };
 
 export default nextConfig; 
